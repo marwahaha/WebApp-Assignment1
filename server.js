6de6139848260d68710bc63ebc19950c21cc376e
Glitch (best-flute) <none>
2018-03-08 17:21:42 +0000

    Import of bmullally/playlist1

--- server.js
+++ server.js
@@ -1,39 +1,24 @@
-// server.js
-// where your node app starts
+'use strict';
 
-// init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const logger = require('./utils/logger');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
+const app = express();
+app.use(cookieParser());
+const exphbs = require('express-handlebars');
+app.use(bodyParser.urlencoded({ extended: false, }));
 app.use(express.static('public'));
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: 'main',
+}));
+app.set('view engine', '.hbs');
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+const routes = require('./routes');
+app.use('/', routes);
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT, function () {
+  logger.info(`glitch-playlist1 started on port ${listener.address().port}`);
 });
